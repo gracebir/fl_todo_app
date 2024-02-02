84bd2762d1905437e1e43ec9fe9f20b73474bddf
gracebir <grace.devolop@gmail.com>
2024-02-02 10:19:04 +0200

    adding checkbox widget

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
 
 class TodoTile extends StatelessWidget {
-  const TodoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+  TodoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(25),
+        padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
         child: Container(
           padding: const EdgeInsets.all(24),
-          child: Text("Make a choise"),
-          decoration: BoxDecoration(color: Colors.yellow),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+            ],
+          ),
+          decoration: const BoxDecoration(
+              color: Colors.yellow,
+              borderRadius: BorderRadius.all(Radius.circular(12))),
         ));
   }
 }
